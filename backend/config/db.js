439dfd3690ef13878d0f518d8925d9334a4af549
rithani <rithaniravi@gmail.com>
2024-09-02 15:27:00 +0530

    change

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ const mongoose=require('mongoose')
 connectDatabase=()=>{
     mongoose.connect(`mongodb+srv://rithaniravi:Rithani2095@clusterbackend.rpda541.mongodb.net/mini-ecommerce`)
     .then(()=>{
-        console.log("DataBase is connected")
+        console.log("DataBase connected")
 })
     .catch((err)=>{
         console.log(err)
